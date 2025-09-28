f6470e5b021895b97c894e372a993bcbf624dde8
MihailMiet <studymiet0@outlook.com>
2025-09-29 00:55:13 +0300

    Lab_v2

--- Lab_1_Alg_MIET_25-26_1/Lab_1_Alg_MIET_25-26_1.c
+++ Lab_1_Alg_MIET_25-26_1/Lab_1_Alg_MIET_25-26_1.c
@@ -1,32 +1,60 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 
-int* num_arr_fill(int N, int num_cnt, int* num_arr) {
-	while (num_cnt < N) {
-		num_arr[num_cnt] = num_cnt;
-		num_cnt++;
-	}
-	return *num_arr;
-}
+int num_match_check(int* num_arr_new, int* N) {
+	int cur_num = 2, cur_check = 2, num_vol = 0;
+	free(num_arr_new);
 
-int simp_num_check(int N, int num_cnt, int* num_arr) {
-	int local_num_cnt = 0;
-	while (1) {
-		num_arr[local_num_cnt]
-	}
-}
-int simp_num_fill(int N, int num_cnt, int* num_arr, int* simp_arr) {
-	int simp_num = 2;
-	while (num_cnt < N) {
-		simp_arr[num_cnt] = simp_num;
-		while (1) {
-		if (num_arr[num_cnt]
+	while (cur_num <= *N) {
+		while (cur_check < cur_num) {
+			if (cur_num % cur_check == 0 && cur_num % cur_check != cur_num) { break; }
+			else { 
+				num_arr_new = (int*)realloc(num_arr_new, sizeof(int) * (num_vol + 1)); 
+				num_arr_new[num_vol] = cur_num;
+				num_vol++;
+			}
+			cur_check++;
 		}
+		cur_num++;
 	}
+	return num_vol + 1;
 }
+
 int main() {
-	int* num_arr = NULL, *simp_arr = NULL, num_cnt = 0, N;
-	while (1) {
+	int N = 1, num_arr_old_len = 0, num_arr_new_len = 0, cur_num_ind = 0;
+	int* num_arr_old = (int*)malloc(sizeof(int)), * num_arr_new = (int*)malloc(sizeof(int));
+	while(N){
+		N = 1;
+		printf("Enter the limit: ");
 		scanf_s("%d", &N);
-		num_arr = (int)malloc(N * sizeof(int));
+		printf("\n");
+		if (N == 0) { return 0; }
+		num_arr_new_len = num_match_check(num_arr_new, &N);
+		num_arr_old = (int*)realloc(num_arr_old, sizeof(int) * num_arr_new_len);
+		while (cur_num_ind < num_arr_new_len) {
+			if (num_arr_new[cur_num_ind] == num_arr_old[cur_num_ind]) {
+				printf("\033[;31;m%d", num_arr_new[cur_num_ind]);
+			}
+			else {
+				printf("\033[;38;m%d", num_arr_new[cur_num_ind]);
+			}
+			cur_num_ind++;
+		}
+		if (num_arr_new_len > num_arr_old_len) {
+			while (cur_num_ind < num_arr_new[cur_num_ind]) {
+				printf("\033[;32;m%d", num_arr_new[cur_num_ind]);
+				num_arr_old[cur_num_ind] = num_arr_new[cur_num_ind];
+				cur_num_ind++;
+			}
+			num_arr_old_len = num_arr_new_len;
+		}
+		cur_num_ind = 0;
+		printf("\n");
+		printf("Press any key to restart: ");
+		scanf_s("%d", &N);
+		printf("\n");
 	}
+	free(num_arr_old);
+	free(num_arr_new);
+	return 0;
 }
