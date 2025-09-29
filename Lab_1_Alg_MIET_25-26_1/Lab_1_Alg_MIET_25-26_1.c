a82e3e59d36b0b19359e5ca01630218c5c44864f
MihailMiet <studymiet0@outlook.com>
2025-09-30 00:09:06 +0300

    Lab_1_v4

--- Lab_1_Alg_MIET_25-26_1/Lab_1_Alg_MIET_25-26_1.c
+++ Lab_1_Alg_MIET_25-26_1/Lab_1_Alg_MIET_25-26_1.c
@@ -5,14 +5,28 @@ int num_match_check(int* num_arr_new, int* N) {
 	int cur_num = 2, cur_check = 2, num_vol = 0;
 	while (cur_num <= *N) {
 		while (cur_check < cur_num) {
-			if (cur_num % cur_check == 0 && cur_num % cur_check != cur_num) { break; }
+			if (cur_num % cur_check == 0 && cur_num != 2) { break; }
 			else { 
-				num_arr_new = (int*)realloc(num_arr_new, sizeof(int) * (num_vol + 1)); 
+				num_vol++;
+			}
+			cur_check++;
+		}
+		cur_check = 2;
+		cur_num++;
+	}
+	cur_num = 2; cur_check = 2;
+	num_arr_new = (int*)malloc(sizeof(int) * num_vol);
+	num_vol = 0;
+	while (cur_num <= *N) {
+		while (cur_check < cur_num) {
+			if (cur_num % cur_check == 0 && cur_num != 2) { break; }
+			else {
 				num_arr_new[num_vol] = cur_num;
 				num_vol++;
 			}
 			cur_check++;
 		}
+		cur_check = 2;
 		cur_num++;
 	}
 	return num_vol + 1;
@@ -20,7 +34,7 @@ int num_match_check(int* num_arr_new, int* N) {
 
 int main() {
 	int N = 1, num_arr_old_len = 0, num_arr_new_len = 0, cur_num_ind = 0;
-	int* num_arr_old = (int*)malloc(sizeof(int)), * num_arr_new = (int*)malloc(sizeof(int));
+	int* num_arr_old = (int*)malloc(sizeof(int)), * num_arr_new = NULL;
 	while(N){
 		N = 1;
 		printf("Enter the limit: ");
@@ -29,6 +43,7 @@ int main() {
 		if (N == 0) { return 0; }
 		num_arr_new_len = num_match_check(num_arr_new, &N);
 		num_arr_old = (int*)realloc(num_arr_old, sizeof(int) * num_arr_new_len);
+		num_arr_new = (int*)realloc(num_arr_new, sizeof(int) * num_arr_new_len);
 		while (cur_num_ind < num_arr_new_len) {
 			if (num_arr_new[cur_num_ind] == num_arr_old[cur_num_ind]) {
 				printf("%d  ", num_arr_new[cur_num_ind]);
